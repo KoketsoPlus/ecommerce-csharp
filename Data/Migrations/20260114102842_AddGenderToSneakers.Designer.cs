1b6a84d232e24b783c64067624638af0222da253
Nomcebo821 <nomcebonkomo203@gmail.com>
2026-01-14 13:04:44 +0200

    Updated layout

--- Data/Migrations/20260114102842_AddGenderToSneakers.Designer.cs
+++ Data/Migrations/20260114102842_AddGenderToSneakers.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ecommerce_csharp.Data;
 
@@ -11,9 +12,11 @@ using ecommerce_csharp.Data;
 namespace ecommerce_csharp.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260114102842_AddGenderToSneakers")]
+    partial class AddGenderToSneakers
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Data/Migrations/20260114105655_newaddings.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ecommerce_csharp.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class newaddings : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
