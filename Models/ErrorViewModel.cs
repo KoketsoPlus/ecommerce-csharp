37703d10a5f35292d9faecef6a0df50f80ba0a00
Koketso Chabalala <kchabalala1105@gmail.com>
2026-01-14 10:28:59 +0200

    initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ecommerce_csharp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
